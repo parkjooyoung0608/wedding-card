66b7a0fe1c4f74175b35c630653ce27e43421469
parkjooyoung <mirkduddl@naver.com>
2025-09-28 11:45:16 +0900

    feat: 신랑/신부 이름 환경변수 추가

---
+++ src/config/config.ts
@@ -0,0 +1,4 @@
+export const GROOM_FULL = import.meta.env.VITE_GROOM_FULL || "신랑";
+export const BRIDE_FULL = import.meta.env.VITE_BRIDE_FULL || "신부";
+export const GROOM_FIRST = import.meta.env.VITE_GROOM_FIRST || "신랑";
+export const BRIDE_FIRST = import.meta.env.VITE_BRIDE_FIRST || "신부";
