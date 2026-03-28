3034cacca9bc1e747c09c5f0d27512ff15b7fe2f
parkjooyoung <mirkduddl@naver.com>
2026-03-29 01:02:23 +0900

    chore: 지연요청 - 섹션 순서 변경 및 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import Play from "@/components/Play";
 import Hero from "@/sections/Hero";
-// import GreetingMessage from "@/sections/GreetingMessage";
+import GreetingMessage from "@/sections/GreetingMessage";
 import WeddingDay from "@/sections/WeddingDay";
-import AboutUs from "@/sections/AboutUs";
-import Timeline from "@/sections/Timeline";
+// import AboutUs from "@/sections/AboutUs";
+// import Timeline from "@/sections/Timeline";
 import Gallery from "@/sections/Gallery";
 import Location from "@/sections/Location";
 import Donation from "@/sections/Donation";
@@ -56,16 +56,14 @@ if (
 
 function App() {
   return (
-    <div className="md:py-12 md:rounded-3xl">
-      <div className="flex flex-col justify-center h-full max-w-md mx-auto md:rounded-3xl shadow-xl w-[100vw] lg:w-[400px] relative bg-white font-suit">
+    <div className="md:py-4 md:rounded-3xl">
+      <div className="flex flex-col justify-center h-full max-w-md mx-auto md:rounded-3xl shadow-xl w-[100vw] lg:w-[400px] relative bg-brandLight font-suit">
         <Play />
         <Hero />
-        {/* <GreetingMessage /> */}
+        <GreetingMessage />
         <WeddingDay />
-        <AboutUs />
-        <Timeline />
-        <Gallery />
         <Location />
+        <Gallery />
         <Donation />
         <GuestBook />
         {/* <RSVP /> */}
